4fc54761d1444e8b860af5cc1d3d4dd92a455730
Camila Vital <camsvital@gmail.com>
2023-05-08 16:28:53 -0300

    projeto completo - testando data-test

--- src/Usuario.jsx
+++ src/Usuario.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 
-function PerfilUsuario(props){
-    const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg")
-    const [nome, setNome] = React.useState("Catana")
+function PerfilUsuario(props) {
+  const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg");
+  const [nome, setNome] = React.useState("Catana");
 
+  function InserirImagem() {
+    const ImagemNova = prompt("Insira URL da foto de perfil nova");
+    if (ImagemNova != "") {
+      setImagem(ImagemNova);
+    }
+  }
 
-function InserirImagem(){
-    const ImagemNova = prompt("Insira URL da foto de perfil nova")
-    setImagem(ImagemNova)
-}
+  function InserirNome() {
+    const NomeNovo = prompt("Insira o novo user");
+    if (NomeNovo != "") {
+      setNome(NomeNovo);
+    }
+  }
 
-function InserirNome(){
-    const NomeNovo = prompt("Insira o novo user")
-    setNome(NomeNovo)
-}
-
-return(
+  return (
     <div class="usuario">
-    <img  onClick = {InserirImagem} src={imagem} />
-    <div class="texto">
-      <strong>{props.usuario}</strong>
-      <span>
-        {nome}
-        <ion-icon onClick = {InserirNome} name="pencil"></ion-icon>
-      </span>
+      <img data-test="profile-image" onClick={InserirImagem} src={imagem} />
+      <div class="texto">
+        <strong>{props.usuario}</strong>
+        <span data-test="name">
+          {nome}
+          <ion-icon data-test="edit-name" onClick={InserirNome} name="pencil"></ion-icon>
+        </span>
+      </div>
     </div>
-  </div> 
-)
+  );
 }
 
 export default function Usuario() {
-    return (
-      <PerfilUsuario usuario = "Catana Comics"  />
-    )
-  }
+  return <PerfilUsuario usuario="Catana Comics" />;
+}
