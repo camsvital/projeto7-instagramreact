3845028ca0de4a273006342a59443c581d2e051b
Camila Vital <camsvital@gmail.com>
2023-05-08 15:30:50 -0300

    data-test

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -22,71 +22,72 @@ function Post(props) {
     }
   }
 
-    return (
-      <div class="post">
-        <div class="topo">
-          <div class="usuario">
-            <img src={props.usuarioFoto} />
-            {props.usuarioTexto}
+  return (
+    <div data-teste="post" class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.usuarioFoto} alt="imagem-usuario"/>
+          {props.usuarioTexto}
+        </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div class="conteudo">
+        <img
+          data-teste="post-image"
+          class={curtir === false ? "" : "colorido"}
+          onClick={CurtirPost}
+          name={curtir === true ? "heart-outline" : "heart"}
+          src={props.imagem}
+        />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon
+              data-teste="like-post"
+              class={curtir === false ? "" : "colorido"}
+              onClick={CurtirPost}
+              name={curtir === true ? "heart-outline" : "heart"}
+            ></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-          <div class="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          <div>
+            <ion-icon
+              data-test="save-post"
+              onClick={SalvarPost}
+              name={salvar === false ? "bookmark-outline" : "bookmark"}
+            ></ion-icon>
           </div>
         </div>
 
-        <div class="conteudo">
-          <img
-            class={curtir === false ? "" : "colorido"}
-            onClick={CurtirPost}
-            name={curtir === false ? "heart-outline" : "heart"}
-            src={props.imagem}
-          />
-        </div>
-
-        <div class="fundo">
-          <div class="acoes">
-            <div>
-              <ion-icon
-                class={curtir === false ? "" : "colorido"}
-                onClick={CurtirPost}
-                name={curtir === false ? "heart-outline" : "heart"}
-              ></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-              <ion-icon
-                onClick={SalvarPost}
-                name={salvar === false ? "bookmark-outline" : "bookmark"}
-              ></ion-icon>
-            </div>
-          </div>
-
-          <div class="curtidas">
-            <img src={props.pagCurtida} />
-            <div class="texto">
-              Curtido por
-              <strong> {props.usuarioCurtida}</strong> e{" "}
-              <strong> outras {curtidas} pessoas</strong>
-            </div>
+        <div class="curtidas">
+          <img src={props.pagCurtida} />
+          <div class="texto">
+            Curtido por
+            <strong> {props.usuarioCurtida}</strong> e{" "}
+            <strong data-test="likes-number"> outras {curtidas} pessoas</strong>
           </div>
         </div>
       </div>
-    );
-  }
-
+    </div>
+  );
+}
 
 export default function Posts() {
-  
-    const ListaPost = [      <Post
+  const ListaPost = [
+    <Post
+      
       usuarioFoto="assets/img/meowed.svg"
       usuarioTexto="meowed"
       imagem="assets/img/gato-telefone.svg"
-      pagCurtida = "assets/img/respondeai.svg"
-      usuario = "respondeai"
-      QtdeCurtidas = {150}
-  
-  
+      pagCurtida="assets/img/respondeai.svg"
+      usuario="respondeai"
+      QtdeCurtidas={150}
     />,
     <Post
       usuarioFoto="assets/img/barked.svg"
@@ -95,11 +96,13 @@ export default function Posts() {
       pagCurtida="assets/img/adorable_animals.svg"
       usuarioCurtida="adorable_animals"
       QtdeCurtidas={35}
-    />
-  ]
-    return (
-      <ul>
-        {ListaPost.map((i) => <li>{i}</li>)}
-      </ul>
-    );
-  }
+    />,
+  ];
+  return (
+    <ul>
+      {ListaPost.map((i) => (
+        <li>{i}</li>
+      ))}
+    </ul>
+  );
+}
